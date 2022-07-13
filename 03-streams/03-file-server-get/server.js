ed6451addda960a65e5d6b748104ff6626031544
Nikolay Kotikov <nikolai.kotikov@gmail.com>
2022-07-13 09:38:44 +0300

    feat: add solution for 3-module 3-task
    Signed-off-by: Nikolay Kotikov <nikolai.kotikov@gmail.com>

--- 03-streams/03-file-server-get/server.js
+++ 03-streams/03-file-server-get/server.js
@@ -1,8 +1,12 @@
-const url = require('url');
 const http = require('http');
 const path = require('path');
+const fs = require('node:fs');
 
 const server = new http.Server();
+
+const hasSubDir = (path) => {
+  return path.includes('/');
+};
 
 server.on('request', (req, res) => {
   const url = new URL(req.url, `http://${req.headers.host}`);
@@ -10,8 +14,31 @@ server.on('request', (req, res) => {
 
   const filepath = path.join(__dirname, 'files', pathname);
 
+
   switch (req.method) {
     case 'GET':
+      if (hasSubDir(pathname)) {
+        res.statusCode = 400;
+        res.end('Bad Request');
+        break;
+      }
+
+      const stream = fs.createReadStream(filepath);
+
+      stream.pipe(res);
+
+      stream.on('error', (err) => {
+        if (err.code === 'ENOENT') {
+          res.statusCode = 404;
+          res.end('File Not Found');
+        } else {
+          res.statusCode = 500;
+          res.end('Internal Server Error');
+        }
+      });
+      req.on('aborted', () => {
+        stream.destroy();
+      });
 
       break;
 
