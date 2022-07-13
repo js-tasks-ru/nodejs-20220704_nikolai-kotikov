e25d0e43d15536a90dc9fc2dc3828cdfd10619c4
Nikolay Kotikov <nikolai.kotikov@gmail.com>
2022-07-13 11:07:14 +0300

    feat: add solution for 3-module 4-task
    Signed-off-by: Nikolay Kotikov <nikolai.kotikov@gmail.com>

--- 03-streams/04-file-server-post/server.js
+++ 03-streams/04-file-server-post/server.js
@@ -1,8 +1,15 @@
 const url = require('url');
 const http = require('http');
 const path = require('path');
+const fs = require('fs');
+const LimitSizeStream = require('./LimitSizeStream');
 
 const server = new http.Server();
+
+const hasSubDir = (slug) => {
+  return slug.includes('/');
+};
+
 
 server.on('request', (req, res) => {
   const url = new URL(req.url, `http://${req.headers.host}`);
@@ -12,6 +19,43 @@ server.on('request', (req, res) => {
 
   switch (req.method) {
     case 'POST':
+      if (hasSubDir(pathname)) {
+        res.statusCode = 400;
+        res.end('Bad Request');
+        break;
+      }
+
+      if (fs.existsSync(filepath)) {
+        res.statusCode = 409;
+        res.end('File Already exists');
+        break;
+      }
+
+      const limitSizeStreamTransform = new LimitSizeStream({limit: 1048576});
+      const fileWriteStream = fs.createWriteStream(filepath);
+      req.pipe(limitSizeStreamTransform).pipe(fileWriteStream);
+
+      limitSizeStreamTransform.on('error', () => {
+        fileWriteStream.destroy();
+        fs.rmSync(filepath);
+        res.statusCode = 413;
+        res.end('Payload Too Large');
+      });
+      fileWriteStream
+          .on('error', () => {
+            fileWriteStream.destroy();
+            res.statusCode = 500;
+            res.end('Internal Server Error');
+          })
+          .on('finish', () => {
+            res.statusCode = 201;
+            res.end('Created');
+          });
+      req.on('aborted', () => {
+        fileWriteStream.destroy();
+        fs.rmSync(filepath);
+      });
+
 
       break;
 
