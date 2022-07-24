018ad20cd890d0ebaa0f49a122ce512531d2896d
Nikolay Kotikov <nikolai.kotikov@gmail.com>
2022-07-24 12:58:25 +0300

    feat: add solution for 6-module 2-task
    Signed-off-by: Nikolay Kotikov <nikolai.kotikov@gmail.com>

--- 06-mongodb-mongoose/02-rest-api/controllers/products.js
+++ 06-mongodb-mongoose/02-rest-api/controllers/products.js
@@ -1,16 +1,33 @@
+const mongoose = require('mongoose');
+const Product = require('../models/Product');
+const mapProduct = require('../mappers/product');
+
+const ObjectId = mongoose.Types.ObjectId;
+
 module.exports.productsBySubcategory = async function productsBySubcategory(ctx, next) {
   const {subcategory} = ctx.query;
 
   if (!subcategory) return next();
 
-  ctx.body = {};
+  const products = await Product.find({subcategory});
+
+  ctx.body = {products: products.map(mapProduct)};
 };
 
 module.exports.productList = async function productList(ctx, next) {
-  ctx.body = {};
+  const products = await Product.find({});
+  ctx.body = {products: products.map(mapProduct)};
 };
 
 module.exports.productById = async function productById(ctx, next) {
-  ctx.body = {};
-};
+  const id = ctx.params.id;
+  if (!ObjectId.isValid(id)) {
+    ctx.throw(400, 'Bad Request');
+  }
+  const product = await Product.findById(id);
 
+  if (!product) {
+    ctx.throw(404, 'Not Found');
+  }
+  ctx.body = {product: mapProduct(product)};
+};
